4de38aab355195517e7d50ed23a02baa8bff0c7a
hakanselim <90356190+hakanselim@users.noreply.github.com>
2021-09-13 00:37:54 +0300

    FirstCommit

---
+++ Assignment_hakanSelim/src/replit/PrintVarialble03.java
@@ -0,0 +1,18 @@
+package replit;
+
+public class PrintVarialble03 {
+
+	public static void main(String[] args) {
+		/* Given two variables num and cost that have been 
+		 * declared and given values, write a statement to print the values.
+		 */
+
+			/*Ex:
+
+			Input: 2 5
+
+			Output: num= 2 cost= 5.0*/
+
+	}
+
+}
